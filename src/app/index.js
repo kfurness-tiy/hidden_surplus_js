d7048b33a698400746d517bd18abc9a5d7b5d3d1
kelly <kelly.n.waite@gmail.com>
2016-12-03 15:30:37 -0700

    WIP Experiment with adding d3

--- src/app/index.js
+++ src/app/index.js
@@ -37,3 +37,88 @@ function getValue () {
 }
 
 document.getElementById('submitForm').onclick = getValue;
+
+
+fbDoGood.on('value', (snapshot) => {
+  //displays entire object
+
+  d3.select(window).on("resize",callFunction);
+  callFunction();
+
+  function callFunction(){
+
+    var tooltip = d3.select("svg").append("div")
+      .style("opacity", "0").style("position", "absolute")
+
+    var svgtest = d3.select('body').select('svg');
+
+    if (!svgtest.empty()) {
+      svgtest.remove();
+    }
+
+    var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    var height = 500;
+
+      var vertices = d3.range(100).map(function(d){ return [Math.random() * width, Math.random() * height]});
+
+      var voronoi = d3.voronoi().size([width,height]);
+
+      var svg = d3.select('body').append('svg').attr("width", '100%').attr('height', '100%');
+
+      var chartGroup = svg.append("g")
+
+
+      //zoom with mouse wheel
+      chartGroup.call(d3.zoom()
+      //how far I can zoom in/out
+      .scaleExtent([0.8,2])
+      .on("zoom", function() {
+        chartGroup.attr("transform",d3.event.transform);
+      }));
+
+      chartGroup.append('g').attr('class','polygons')
+      .selectAll('path')
+      .data(voronoi.polygons(vertices))
+      .enter().append('path')
+        .attr("d",function(d){return "M"+d.join("L")+"Z";})
+        .on("mousemove", function(d){
+          tooltip.style("opacity", "1")
+          .style("left",d[0][0]+"px")
+          .style("top",d[0][1]+"px")
+          // .style("left",d3.event.pageX+"px")
+          // .style("top",d3.event.pageY+"px")
+          // console.log(d3.event); Good to see all properties of mouse event
+          tooltip.html("Number of sides: " + d.length);
+        })
+        // .on("mouseover", function(){this.style.fill =
+        // "red"})
+        // .on("mouseout", function(){this.style.fill = "white"})
+
+
+
+
+      chartGroup.append('g').attr("class", "fuel")
+        .selectAll('circle')
+          .data(vertices)
+            .enter().append('circle')
+              .attr("cx", function(d){return d[0]})
+              .attr("cy", function(d){return d[1]})
+              .attr("r","2.5");
+
+    d3.select("g.polygons").select("path:nth-child(30)")
+      // can have several transitions!
+      // .transition().duration(1000)
+      // .style("fill", "red")
+      .transition().duration(2000)
+        // .delay(1000)
+      .style("fill","blue").style("opacity","0.7")
+      .attr("transform","translate(10,10)")
+
+
+
+    d3.select("g.polygons").select("path:nth-child(30)")
+      .dispatch("mousemove");
+
+  }
+
+});
