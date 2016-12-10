50f556389d63451109e7eb57216c2edf1a8f39d4
kelly <kelly.n.waite@gmail.com>
2016-12-09 21:20:05 -0700

    Add styling for mobile responsiveness

--- src/app/firebase_auth.js
+++ src/app/firebase_auth.js
@@ -17,8 +17,8 @@ var config = {
 ///////////////// Authentication /////////////////////
 
 $(document).ready(function (){
-  $("#form").hide();     // CHANGE
-  // $("#logIn").hide();    // CHANGE
+  $("#form").show();     // CHANGE
+  $("#logIn").hide();    // CHANGE
   $("#signOutNav").hide();
   $("#signOutMsg").hide();
   $("#formSubmitMsg").hide();
