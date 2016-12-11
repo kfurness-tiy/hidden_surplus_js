d1209a1813a990c64a79554deb003266f24b7e33
kelly <kelly.n.waite@gmail.com>
2016-12-11 11:42:30 -0700

    WIP Generate data and create pie chart

--- src/app/where_to_donate_graph.js
+++ src/app/where_to_donate_graph.js
@@ -17,7 +17,6 @@ fbDoGood.on('value', (snapshot) => {
       selectWhere.push(c.selectWhere);
     })
 
-    console.log(selectWhere);
 
 
     let width = $(window).innerWidth() * 0.40,
@@ -27,17 +26,48 @@ fbDoGood.on('value', (snapshot) => {
     let color = d3.scaleOrdinal()
         .range(['rgba(30,156,150,0.2)', 'rgba(115,191,184,0.2)', 'rgba(59,102,112,0.2)', 'rgba(158,157,154,0.2)', 'rgba(80,163,153,0.2)']);
 
+        function genData () {
+          let where = ['charity', 'family', 'school', 'fundraiser', 'other']
+
+          let dataset = new Array();
+
+          where.map((c,i,a) => {
+            let total = 0;
+            selectWhere.map((j) => {
+              if (c === j) {
+                total += 1;
+              }
+            })
+            let obj = {"category": c, "total": total}
+            dataset.push(obj);
+          })
+          console.log(dataset);
+          return dataset;
+        }
+
+        genData();
+
     let svg = d3.select("#whereDonateChartDiv").append("svg")
       .attr("width", width)
-      .attr("height", height)
+      .attr("height", height);
+
+    var chartGroup = svg.append("g");
 
     let arc = d3.arc()
       .outerRadius(radius - 10)
       .innerRadius(radius - 50)
+      .startAngle(0)
+      .endAngle(Math.PI / 2);
 
     let pie = d3.pie()
       .sort(null)
       .value(function(d) {console.log(d);})
+
+
+    chartGroup.selectAll("arc")
+        .data(pie(genData))
+      .enter().append("g")
+        .attr("class", "arc");
 
   }
 
